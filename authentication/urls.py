e42dc531ab804644cbc143a7ea252fe6f92f180b
dasUnicorn <info@dasunicorn.de>
2023-12-26 08:37:13 +0100

    feat: creating the option for users to view their profile, wip, #1

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('profile/<str:user>', views.UserProfile.as_view(), name='profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Profile
+
+class UserProfile(generic.ListView, user):
+    queryset = Profile.objects.filter(user=user)
+    template_name = "profile.html"
