fbab5eb64b653217c2e3b55066101820b75cfc24
dasUnicorn <info@dasunicorn.de>
2023-12-31 16:38:57 +0100

    feat: add styling to navbar and header

--- wordweave/settings.py
+++ wordweave/settings.py
@@ -12,14 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = BASE_DIR / 'assets'
 MEDIA_URL = '/img/'
+
+
 
 
 # Quick-start development settings - unsuitable for production
