3e86959a1ac17298cde6b661d4ac5cd446874341
dasUnicorn <info@dasunicorn.de>
2024-01-05 15:02:18 +0100

    feat: update admin interface with new custom user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
+
+class ProfileAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Profile"), {"fields": ("bio", "email", "profile_pic")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User, ProfileAdmin)
