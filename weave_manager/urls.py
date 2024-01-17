4d1160094bec312e5eaebb921fd273291ee546d3
dasUnicorn <info@dasunicorn.de>
2024-01-17 21:38:31 +0100

    feat: add new threads on the homepage, #2

--- weave_manager/urls.py
+++ weave_manager/urls.py
@@ -1,6 +1,8 @@
 from . import views
+from .views import CreateThreadView
 from django.urls import path
 
 urlpatterns = [
     path('', views.GlobalTimeline.as_view(), name='home'),
+    path('create_thread', CreateThreadView.as_view(), name='create_thread'),
 ]
--- weave_manager/views.py
+++ weave_manager/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Thread
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from .forms import ThreadForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -9,3 +14,15 @@ class GlobalTimeline(generic.ListView):
     queryset = Thread.objects.all()
     template_name = "index.html"
     paginate_by = 6
+
+#  LoginRequiredMixin is used to ensure that only logged-in users can create threads.
+class CreateThreadView(LoginRequiredMixin, FormView):
+    template_name = 'create_thread.html'
+    form_class = ThreadForm
+    success_url = reverse_lazy('home')  # Redirect to a view that displays a list of threads
+
+    def form_valid(self, form):
+        # Set the author before saving the form
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
