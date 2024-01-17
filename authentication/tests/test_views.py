a65a1600f1740c2e35b136f089cbc2d3582e7541
dasUnicorn <info@dasunicorn.de>
2024-01-17 20:32:52 +0100

    feat: unittest for authentication view is finally done, #34

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,22 +1,43 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from authentication.models import User
+from django.contrib import auth
 import json
 
 class TestViews(TestCase):
 
 	# Setting up the Test-Data
 	def setUp(self):
+		# Create Test User
+		self.user1 = User.objects.create(username='user1')
+		self.user1.set_password('testpw!')
+		self.user1.save()
+
+		# Create Client
 		self.client = Client()
-		self.user1 = User.objects.create(
-			username = 'user1',
-			bio = 'This is my bio.'
-			)
-		self.profile_url = reverse('user_profile', kwargs={'username': 'user1'})
 
-	# Testing to get profile page
-	def test_profile_GET(self):
+		# Set up Profile url
+		self.profile_url = reverse('user_profile', args=['user1'])
+
+	# Testing to get profile page when logged in
+	def test_profile_get_logged_in(self):
+		# Checking if User is logged in
+		logged_in = self.client.login(username='user1', password='testpw!')
+		user = auth.get_user(self.client)
+		self.assertEqual(True, user.is_authenticated)
+
+		# Testing profile View
 		response = self.client.get(self.profile_url)
-
 		self.assertEqual(response.status_code, 200)
-		self.asserrTemplateUsed(response, 'authentication/templates/profile.html')
+		self.assertTemplateUsed(response, 'profile.html')
+
+	# Testing to get profile page when NOT logged in
+	def test_profile_get_logged_out(self):
+		# Checking if User is logged out
+		user = auth.get_user(self.client)
+		self.assertEqual(False, user.is_authenticated)
+
+		# Testing Profile View
+		response = self.client.get(self.profile_url)
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'profile.html')
