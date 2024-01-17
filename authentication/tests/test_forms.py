8ea44a93fe299773d0d733e6ea4745fd3e59e5fc
dasUnicorn <info@dasunicorn.de>
2024-01-17 16:33:08 +0100

    feat: unittest to check form input for authentication, #31

---
+++ authentication/tests/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from authentication.forms import ProfileUpdateForm
+
+class TestForms(SimpleTestCase):
+
+	# Testing bio form with valid bio text.
+	def test_profile_update_form_valid_data(self):
+		form = ProfileUpdateForm(data={
+			'bio': 'This is your new bio.'
+			})
+		self.assertTrue(form.is_valid())
+
+	# Testing bio with invalid data (201 characters)
+	def test_profile_update_form_invalid_data(self):
+		form = ProfileUpdateForm(data={
+			'bio': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e'
+			})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(len(form.errors), 1)
+
