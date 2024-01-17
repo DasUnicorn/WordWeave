a280c420ea5c3f9f91dacef167324c94d813c76b
dasUnicorn <info@dasunicorn.de>
2024-01-17 21:49:15 +0100

    style: add crispy forms to thread creation, #2

--- weave_manager/forms.py
+++ weave_manager/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Thread
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class ThreadForm(forms.ModelForm):
     class Meta:
         model = Thread
         fields = ['title', 'content', 'tags']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Create Thread'))
