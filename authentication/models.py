547ee8f3d001ed25f2db5eb8baf60eb4563369ac
dasUnicorn <info@dasunicorn.de>
2024-01-30 01:36:36 +0100

    feat: add default bio text, #47

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    bio = models.CharField(max_length=200, unique=False)
+    bio = models.CharField(max_length=200, unique=False, default="Your bio could be here.")
     created_on = models.DateTimeField(auto_now_add=True)
     profile_pic = models.ImageField(null=True, blank=True, default="default.png")
