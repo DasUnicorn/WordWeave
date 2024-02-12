113e4f92152d30a64ad301e887730d50e59ebade
dasUnicorn <info@dasunicorn.de>
2024-02-12 21:31:35 +0100

    fix: delete old profile pictures on save, #57

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.core.files.storage import default_storage
+from django.core.exceptions import ObjectDoesNotExist
 
 class User(AbstractUser):
     bio = models.CharField(max_length=200, unique=False, default="Your bio could be here.")
@@ -8,17 +10,18 @@ class User(AbstractUser):
     profile_pic = models.ImageField(null=True, blank=True, default="default.png", upload_to='profiles')
 
     def save(self, *args, **kwargs):
-        # Check if the instance has a primary key (i.e., if it's an existing user)
+        # Check if the instance has a primary key
         if self.pk:
             try:
                 current_user = User.objects.get(pk=self.pk)
                 # If the profile picture exist and has changed, delete the old one
                 if current_user.profile_pic and self.profile_pic != current_user.profile_pic:
-                    # Delete the old profile picture from storage bucket
-                    if os.path.isfile(current_user.profile_pic.path):
-                        os.remove(current_user.profile_pic.path)
-            except User.DoesNotExist:
+                    # Delete the old profile picture from the Cloudflare R2 bucket
+                    try:
+                        default_storage.delete(current_user.profile_pic.name)
+                    except FileNotFoundError:
+                        pass  # If the file does not exist, do nothing
+            except ObjectDoesNotExist:
                 raise ValueError("User does not exist")
-        # Save the new profile picture
         super().save(*args, **kwargs)
 
