efb1385c7eb111400b859d1031e01b3e98e4e70c
dasUnicorn <info@dasunicorn.de>
2024-02-12 21:20:30 +0100

    feat: delete old profile pictures on save, #57

--- authentication/models.py
+++ authentication/models.py
@@ -6,3 +6,19 @@ class User(AbstractUser):
     bio = models.CharField(max_length=200, unique=False, default="Your bio could be here.")
     created_on = models.DateTimeField(auto_now_add=True)
     profile_pic = models.ImageField(null=True, blank=True, default="default.png", upload_to='profiles')
+
+    def save(self, *args, **kwargs):
+        # Check if the instance has a primary key (i.e., if it's an existing user)
+        if self.pk:
+            try:
+                current_user = User.objects.get(pk=self.pk)
+                # If the profile picture exist and has changed, delete the old one
+                if current_user.profile_pic and self.profile_pic != current_user.profile_pic:
+                    # Delete the old profile picture from storage bucket
+                    if os.path.isfile(current_user.profile_pic.path):
+                        os.remove(current_user.profile_pic.path)
+            except User.DoesNotExist:
+                raise ValueError("User does not exist")
+        # Save the new profile picture
+        super().save(*args, **kwargs)
+
