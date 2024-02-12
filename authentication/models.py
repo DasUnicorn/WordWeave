414e451cd864cf93326680ca0191430ed0cbbe2a
dasUnicorn <info@dasunicorn.de>
2024-02-12 16:43:11 +0100

    feat: upload user profiles to R2 storage, #57

--- authentication/models.py
+++ authentication/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.CharField(max_length=200, unique=False, default="Your bio could be here.")
     created_on = models.DateTimeField(auto_now_add=True)
-    profile_pic = models.ImageField(null=True, blank=True, default="default.png")
+    profile_pic = models.ImageField(null=True, blank=True, default="default.png", upload_to='assets')
