ef531d24e49b7c6bbf5aada427ceb18d759d8eb4
dasUnicorn <info@dasunicorn.de>
2024-02-17 21:29:18 +0100

    fix: finally solved error when clearing profile pictures, #26

--- authentication/models.py
+++ authentication/models.py
@@ -9,20 +9,3 @@ class User(AbstractUser):
     bio = models.CharField(max_length=200, null=True, blank=True, unique=False, default="Your bio could be here.")
     created_on = models.DateTimeField(auto_now_add=True)
     profile_pic = models.ImageField(null=True, blank=True, upload_to='profiles')
-
-    def save(self, *args, **kwargs):
-        # Check if the instance has a primary key
-        if self.pk:
-            try:
-                current_user = User.objects.get(pk=self.pk)
-                # If the profile picture exist and has changed, delete the old one
-                if current_user.profile_pic and self.profile_pic != current_user.profile_pic:
-                    # Delete the old profile picture from the Cloudflare R2 bucket
-                    try:
-                        default_storage.delete(current_user.profile_pic.name)
-                    except FileNotFoundError:
-                        pass  # If the file does not exist, do nothing
-            except ObjectDoesNotExist:
-                raise ValueError("User does not exist")
-        super().save(*args, **kwargs)
-
