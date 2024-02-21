d42135785fddf9def3dcedeb0b8af90c8cb734ad
dasUnicorn <info@dasunicorn.de>
2024-02-21 12:43:00 +0100

    style: adjust pep8

--- follow/views.py
+++ follow/views.py
@@ -1,54 +1,56 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.views import generic
 from .models import Follower
 from taggit.models import Tag
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 
 
 def follow_tag(request):
     if request.method == 'POST':
         tag_name = request.POST.get('tag_name')
-
         if tag_name and request.user.is_authenticated:
-            tag_to_follow, created = Tag.objects.get_or_create(name=tag_name)
-
-            # Check if the user is already following the tag
-            if Follower.objects.filter(followed_by=request.user,
-                                       following=tag_to_follow).exists():
-                messages.warning(request, f'You are already following {tag_to_follow.name}.')
-            else:
-                Follower.objects.create(
-                    followed_by=request.user, following=tag_to_follow)
-                messages.success(request, f'You are now following {tag_to_follow.name}.')
+            try:
+                # Attempt to retrieve the tag from the database
+                tag_to_follow = Tag.objects.get(name=tag_name)
+                # Check if the user is already following the tag
+                if Follower.objects.filter(followed_by=request.user,
+                                           following=tag_to_follow).exists():
+                    messages.warning(request, f'You are already following {tag_to_follow.name}.')
+                else:
+                    Follower.objects.create(
+                        followed_by=request.user, following=tag_to_follow)
+                    messages.success(request, f'You are now following {tag_to_follow.name}.')
+                return redirect('tag_site', slug=tag_to_follow.slug)
+            except Tag.DoesNotExist:
+                # If the tag does not exist, redirect to the home page
+                messages.error(request, 'Invalid tag name.')
+                return redirect('home')
         else:
             messages.error(request, 'Invalid tag name.')
-
-    return redirect('tag_site', slug=tag_to_follow.slug)
+    return redirect('home')  # Redirect to home if user is not authenticated
 
 
 def unfollow_tag(request):
     if request.method == 'POST':
         tag_name = request.POST.get('tag_name')
-        print(f'Tag name from form: {tag_name}')
-
         if tag_name and request.user.is_authenticated:
-            tag = get_object_or_404(Tag, name=tag_name)
-
-            if Follower.objects.filter(followed_by=request.user,
-                                       following=tag).exists():
-                Follower.objects.filter(
-                    followed_by=request.user, following=tag).delete()
-                messages.success(request, f'You have unfollowed {tag.name}.')
-            else:
-                messages.warning(request, f'You are not following {tag.name}.')
+            try:
+                tag = Tag.objects.get(name=tag_name)
+                if Follower.objects.filter(followed_by=request.user,
+                                           following=tag).exists():
+                    Follower.objects.filter(
+                        followed_by=request.user, following=tag).delete()
+                    messages.success(request, f'You have unfollowed {tag.name}.')
+                else:
+                    messages.warning(request, f'You are not following {tag.name}.')
+                return redirect('tag_site', slug=tag.slug)
+            except Tag.DoesNotExist:
+                messages.error(request, 'Invalid tag name.')
         else:
             messages.error(
                 request, 'Invalid tag name or user not authenticated.')
-
-    return redirect('tag_site', slug=tag.slug)
+    # Redirect to home if tag is invalid or user is not authenticated
+    return redirect('home')
 
 
 class UserTagView(generic.ListView):
