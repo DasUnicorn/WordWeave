5c2e8ef0acb62aeb0c49897ed7b0b65ff8c74ead
dasUnicorn <info@dasunicorn.de>
2024-02-21 11:47:55 +0100

    test: add unittest for models in app auth, #30

--- authentication/tests/test_models.py
+++ authentication/tests/test_models.py
@@ -1,13 +1,26 @@
-from django.test import TestCase, Client
-from django.urls import reverse
+from django.test import TestCase
 from authentication.models import User
 
 class TestModals(TestCase):
 
-	# Set up user info, for further test cases
-	def setUp(self):
-		self.user1 = User.objects.create(
-			username = 'Salbei',
-			bio = 'This is a bio.',
-			email = 'test@test.de'
-			)
+    # Set up user info for further testing
+    def setUp(self):
+        self.username = 'Salbei'
+        self.bio = 'This is a bio.'
+        self.email = 'test@test.de'
+
+    def test_user_creation(self):
+        # Create a user instance
+        user = User.objects.create(
+            username=self.username,
+            bio=self.bio,
+            email=self.email
+        )
+
+        # Assert that the user object is created successfully
+        self.assertIsNotNone(user)
+
+        # Assert that the attributes of the user object match the expected values
+        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.bio, self.bio)
+        self.assertEqual(user.email, self.email)
