90fac9a74a94ddae6feae049e2205db1f0f2bd07
dasUnicorn <info@dasunicorn.de>
2024-02-22 20:54:21 +0100

    fix: remove check that is now handle by django clean up

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):  # noqa: F811
+class User(AbstractUser):
     username = models.CharField(max_length=25, unique=True)
     bio = models.CharField(max_length=200, null=True, blank=True, unique=False,
                            default="Your bio could be here.")
